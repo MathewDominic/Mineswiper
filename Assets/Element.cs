1d5aa7653cfd178baec5574bd093361f5ca7d496
Mathew Dominic Puthoor <mathew@playsimple.in>
2017-04-23 18:01:46 +0530

    Adding new files

---
+++ Assets/Element.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Element : MonoBehaviour {
+
+	// Is this a mine?
+	public bool mine;
+
+	// Different Textures
+	public Sprite[] emptyTextures;
+	public Sprite[] activeTextures;
+	public Sprite mineTexture;
+	public Sprite defaultTexture;
+	public Sprite flagTexture;
+	public int x,y;
+
+	// Use this for initialization
+	void Start () {
+		// Randomly decide if it's a mine or not
+		init();
+	}
+	public void init() {
+		mine = Random.value < 0.15;
+
+		x = (int)transform.position.x;
+		y = (int)transform.position.y;
+		Grid.elements[x, y] = this;
+	}
+	public void loadTexture(int adjacentCount, bool activeFlag=false) {
+		if (adjacentCount == -1)
+			GetComponent<SpriteRenderer>().sprite = defaultTexture;
+		else if (adjacentCount == -2)
+			GetComponent<SpriteRenderer>().sprite = flagTexture;
+		else if (mine)
+			GetComponent<SpriteRenderer>().sprite = mineTexture;
+		else if (activeFlag)
+			GetComponent<SpriteRenderer>().sprite = activeTextures[adjacentCount];
+		else
+			GetComponent<SpriteRenderer>().sprite = emptyTextures[adjacentCount];
+	}
+	void Update() {
+		if (Input.GetKeyDown ("up") && Grid.currY < Grid.h - 1 && Grid.upPressed == false) {
+//			Grid.resetGrid ();
+			Grid.FFuncover(Grid.currX, Grid.currY, new bool[Grid.w, Grid.h]);
+			Grid.currY += 1;
+			Grid.FFuncover(Grid.currX, Grid.currY, new bool[Grid.w, Grid.h]);
+//			Grid.FFuncover(Grid.currX+1, Grid.currY);
+//			Grid.FFuncover(Grid.currX-1, Grid.currY);
+//			Grid.FFuncover(Grid.currX, Grid.currY+1);
+//			Grid.FFuncover(Grid.currX, Grid.currY-1);
+			Grid.upPressed = true;
+		}
+		if (Input.GetKeyUp ("up")) {
+			Grid.upPressed = false;
+		}
+		if (Input.GetKeyDown ("down") && Grid.currY > 0 && Grid.downPressed == false) {
+//			Grid.resetGrid ();
+			Grid.FFuncover(Grid.currX, Grid.currY, new bool[Grid.w, Grid.h]);
+			Grid.currY -= 1;
+			Grid.FFuncover(Grid.currX, Grid.currY, new bool[Grid.w, Grid.h]);
+//			Grid.FFuncover(Grid.currX+1, Grid.currY);
+//			Grid.FFuncover(Grid.currX-1, Grid.currY);
+//			Grid.FFuncover(Grid.currX, Grid.currY+1);
+//			Grid.FFuncover(Grid.currX, Grid.currY-1);
+			Grid.downPressed = true;
+		}
+		if (Input.GetKeyUp ("down")) {
+			Grid.downPressed = false;
+		}
+
+
+		if (Input.GetKeyDown ("left") && Grid.currX >0 && Grid.leftPressed == false) {
+//			Grid.resetGrid ();
+			Grid.FFuncover(Grid.currX, Grid.currY, new bool[Grid.w, Grid.h]);
+			Grid.currX -= 1;
+			Grid.FFuncover(Grid.currX, Grid.currY, new bool[Grid.w, Grid.h]);
+//			Grid.FFuncover(Grid.currX+1, Grid.currY);
+//			Grid.FFuncover(Grid.currX-1, Grid.currY);
+//			Grid.FFuncover(Grid.currX, Grid.currY+1);
+//			Grid.FFuncover(Grid.currX, Grid.currY-1);
+			Grid.leftPressed = true;
+		}
+		if (Input.GetKeyUp ("left")) {
+			Grid.leftPressed = false;
+		}
+		if (Input.GetKeyDown ("right") && Grid.currX < Grid.w - 1 && Grid.rightPressed == false) {
+//			Grid.resetGrid ();
+			Grid.FFuncover(Grid.currX, Grid.currY, new bool[Grid.w, Grid.h]);
+			Grid.currX += 1;
+			Grid.FFuncover(Grid.currX, Grid.currY, new bool[Grid.w, Grid.h]);
+//			Grid.FFuncover(Grid.currX+1, Grid.currY);
+//			Grid.FFuncover(Grid.currX-1, Grid.currY);
+//			Grid.FFuncover(Grid.currX, Grid.currY+1);
+//			Grid.FFuncover(Grid.currX, Grid.currY-1);
+			Grid.rightPressed = true;
+		}
+		if (Input.GetKeyUp ("right")) {
+			Grid.rightPressed = false;
+		}
+		
+		Grid.moveCursor ();
+	}
+	void OnMouseUpAsButton() {
+		// It's a mine
+		if (mine) {
+			// Uncover all mines
+			Grid.uncoverMines();
+
+			// game over
+			print("you lose");
+		}
+		// It's not a mine
+		else {
+			// ToDo show adjacent mine number
+			int x = (int)transform.position.x;
+			int y = (int)transform.position.y;
+			loadTexture(Grid.adjacentMines(x, y));
+
+
+			// ToDo uncover area without mines
+			Grid.FFuncover(x, y, new bool[Grid.w, Grid.h]);
+
+
+			// ToDo find out if the game was won now
+			// ...
+		}
+	}
+	void OnMouseOver () {
+		if(Input.GetMouseButtonDown(1)) {
+			loadTexture (-2);
+		}
+	}
+
+
+}
---
+++ Assets/Grid.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid  {
+	public static int w = 8;
+	public static int h = 8;
+	public static int currX = 0;
+	public static int currY = 0;
+	public static Element[,] elements = new Element[w,h];
+
+	public static bool upPressed;
+	public static bool downPressed;
+	public static bool leftPressed;
+	public static bool rightPressed;
+
+	public static void uncoverMines() {
+		foreach (Element elem in elements)
+			if (elem.mine)
+				elem.loadTexture(0);
+	}
+	public static bool mineAt(int x, int y) {
+		// Coordinates in range? Then check for mine.
+		if (x >= 0 && y >= 0 && x < w && y < h)
+			return elements[x, y].mine;
+		return false;
+	}
+	// Count adjacent mines for an element
+	public static int adjacentMines(int x, int y) {
+		int count = 0;
+
+		if (mineAt(x,   y+1)) ++count; // top
+		if (mineAt(x+1, y+1)) ++count; // top-right
+		if (mineAt(x+1, y  )) ++count; // right
+		if (mineAt(x+1, y-1)) ++count; // bottom-right
+		if (mineAt(x,   y-1)) ++count; // bottom
+		if (mineAt(x-1, y-1)) ++count; // bottom-left
+		if (mineAt(x-1, y  )) ++count; // left
+		if (mineAt(x-1, y+1)) ++count; // top-left
+
+		return count;
+	}
+//	public static void FFuncover(int x, int y) {
+//		// Coordinates in Range?
+//		if (x >= 0 && y >= 0 && x < w && y < h) {
+//			// visited already?
+//			if (mineAt(x, y))
+//				return;
+//
+//			// uncover element
+//			elements[x, y].loadTexture(adjacentMines(x, y));
+//
+//			// close to a mine? then no more work needed here
+////			if (adjacentMines(x, y) > 0)
+////				return;
+//
+//			// set visited flag
+////			visited[x, y] = true;
+//
+//			// recursion
+////			FFuncover(x-1, y, visited);
+////			FFuncover(x+1, y, visited);
+////			FFuncover(x, y-1, visited);
+////			FFuncover(x, y+1, visited);
+//		}
+//	}
+
+	public static void FFuncover(int x, int y, bool[,] visited) {
+		// Coordinates in Range?
+		if (x >= 0 && y >= 0 && x < w && y < h) {
+			// visited already?
+
+			if (visited[x, y])
+				return;
+
+			// uncover element
+			elements[x, y].loadTexture(adjacentMines(x, y));
+
+			// close to a mine? then no more work needed here
+			if (adjacentMines(x, y) > 0)
+				return;
+
+			// set visited flag
+			visited[x, y] = true;
+
+			// recursion
+			FFuncover(x-1, y, visited);
+			FFuncover(x+1, y, visited);
+			FFuncover(x, y-1, visited);
+			FFuncover(x, y+1, visited);
+		}
+	}
+	public static void moveCursor() {
+		if (mineAt (currX, currY)) {
+			uncoverMines ();
+			return;
+		}
+		foreach (Element elem in elements) {
+			if (elem.x == currX && elem.y == currY) {
+				elem.loadTexture(adjacentMines(currX, currY),true);	
+			} 
+
+		}	
+	}
+	public static void resetGrid() {
+		foreach (Element elem in elements) {
+			if (elem.x == currX && elem.y == currY) {
+				elem.loadTexture(adjacentMines(currX, currY),true);	
+			} 
+		}	
+	}
+	public static void newGame() {
+		currX = 0;
+		currY = 0;
+//		FFuncover(currX+1, currY);
+//		FFuncover(currX-1, currY);
+//		FFuncover(currX, currY+1);
+//		FFuncover(currX, currY-1);
+
+		foreach (Element elem in elements) {
+			if (elem.x == currX && elem.y == currY) {
+				elem.loadTexture(1,true);	
+			} else 
+				elem.loadTexture(-1);
+			elem.init ();
+		}
+		FFuncover(currX, currY, new bool[Grid.w, Grid.h]);
+	}
+}
